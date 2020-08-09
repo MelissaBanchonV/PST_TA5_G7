dbb6ea9407190091169c8eb00e7ae8be12e92d1a
BrandonLR1999 <BrandonLR1999>
2020-08-08 21:20:39 -0500

    agrego splashactiviy

---
+++ app/src/main/java/com/example/amst7/SplashActiviy.java
@@ -0,0 +1,29 @@
+package com.example.amst7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActiviy extends Activity {
+    private final int tiemposplas=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_activiy);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActiviy.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, tiemposplas);
+    }
+}
